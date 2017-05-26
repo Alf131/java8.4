0b0e2fd1068ed51f0d07abdcb0039e49f165c386
Alf Chlopecki <chlopecki13@gmail.com>
2017-05-26 20:02:55 +0200

    Added 2 comments in line 6 and 7 disabling function console

--- js/index.js
+++ js/index.js
@@ -3,4 +3,5 @@
 var name = prompt('Enter your name');
 // Below I have commented just for learning process
 alert('Hello, ' + name);
-console.log('Hello, ' + name);
+// Below I have commented on a function c to disable it
+// console.log('Hello, ' + name);
